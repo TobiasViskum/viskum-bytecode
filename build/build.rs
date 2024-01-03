690776a0d83c0d9bfb9e2ada47cccd2e214130d2
TobiasViskum <tobias.t.viskum@gmail.com>
2024-01-03 22:00:44 +0100

    Added opcodes: add, subtraction, division, multiplication

--- build/build.rs
+++ build/build.rs
@@ -3,7 +3,16 @@ mod generate_opcode;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let opcodes = ["OpReturn = 0", "OpConstant = 1", "OpConstantLong = 2", "OpNegate = 3"];
+    let opcodes = [
+        "OpReturn = 0",
+        "OpConstant = 1",
+        "OpConstantLong = 2",
+        "OpNegate = 3",
+        "OpAdd = 4",
+        "OpSubtract = 5",
+        "OpMultiply = 6",
+        "OpDivide = 7",
+    ];
 
     generate_opcode::generate_opcode(&opcodes)?;
 
--- build/generate_opcode.rs
+++ build/generate_opcode.rs
@@ -14,6 +14,36 @@ pub fn generate_opcode(opcodes: &[&str]) -> io::Result<()> {
     }
     writeln!(file, "}}")?;
 
+    writeln!(file, "impl OpCode {{")?;
+    writeln!(file, "    pub fn to_string(&self) -> String {{")?;
+    writeln!(file, "        match self {{")?;
+    for opcode in opcodes {
+        let split = opcode.split(" = ").collect::<Vec<&str>>();
+        let opcode = split[0].trim();
+
+        //Replaces any uppercase letters with a space and the corresponding lowercase letter.
+        let display_text = opcode
+            .replace("Op", "")
+            .chars()
+            .enumerate()
+            .map(|(i, c)| {
+                if i == 0 {
+                    c.to_lowercase().to_string()
+                } else if c.is_uppercase() {
+                    format!(" {}", c.to_lowercase())
+                } else {
+                    c.to_string()
+                }
+            })
+            .collect::<Vec<String>>()
+            .join("");
+
+        writeln!(file, "            OpCode::{} => \"{}\".to_string(),", opcode, display_text)?;
+    }
+    writeln!(file, "        }}")?;
+    writeln!(file, "    }}")?;
+    writeln!(file, "}}")?;
+
     writeln!(file, "impl From<u8> for OpCode {{")?;
     writeln!(file, "    fn from(byte: u8) -> Self {{")?;
     writeln!(file, "        match byte {{")?;
@@ -36,3 +66,22 @@ pub fn generate_opcode(opcodes: &[&str]) -> io::Result<()> {
 
     Ok(())
 }
+
+/*
+
+impl OpCode {
+    pub fn to_string(&self) -> String {
+        match self {
+            OpCode::OpReturn => "return".to_string(),
+            OpCode::OpConstant => "constant".to_string(),
+            OpCode::OpConstantLong => "constantlong".to_string(),
+            OpCode::OpNegate => "negate".to_string(),
+            OpCode::OpAdd => "add".to_string(),
+            OpCode::OpSubtract => "subract".to_string(),
+            OpCode::OpMultiply => "multiply".to_string(),
+            OpCode::OpDivide => "divide".to_string(),
+        }
+    }
+}
+
+*/
